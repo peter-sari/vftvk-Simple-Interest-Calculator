ea99fc8ccee18c9b807343fc9d8b3c4f05a83a96
Peter Sari <43958153+peter-sari@users.noreply.github.com>
2021-04-23 23:06:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,15 +19,20 @@ function createyears() {
 
 function compute() {
   let deposit = parseInt(document.getElementById("principal").value);
-  document.getElementById("principal").value = "";
-  let interestrate = parseFloat(document.getElementById("intrate").value);
-  let years = parseInt(document.getElementById("years").value);
-  let amountof = deposit * Math.pow(1 + interestrate / 100, years);
-  let yearof = parseInt(new Date().getFullYear()) + years;
+  if (deposit > 0) {
+    document.getElementById("principal").value = "";
+    let interestrate = parseFloat(document.getElementById("intrate").value);
+    let years = parseInt(document.getElementById("years").value);
+    let amountof = deposit * Math.pow(1 + interestrate / 100, years);
+    let yearof = parseInt(new Date().getFullYear()) + years;
 
-  document.getElementById("result").style.display = "block";
-  document.getElementById("deposit").innerHTML = deposit;
-  document.getElementById("interestrate").innerHTML = interestrate;
-  document.getElementById("amountof").innerHTML = amountof.toFixed(2);
-  document.getElementById("yearof").innerHTML = yearof;
+    document.getElementById("result").style.display = "block";
+    document.getElementById("deposit").innerHTML = deposit;
+    document.getElementById("interestrate").innerHTML = interestrate;
+    document.getElementById("amountof").innerHTML = amountof.toFixed(2);
+    document.getElementById("yearof").innerHTML = yearof;
+  } else {
+    alert("Please use a positive number");
+    document.getElementById("principal").focus();
+  }
 }
